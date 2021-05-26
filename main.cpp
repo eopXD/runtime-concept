d40ae268ac61be7dbf499fcb68d96cb5dbaf3daf
eopXD <eopXD@skymizer.com>
2021-05-26 17:34:27 +0800

    providing move constructor and move assignment avoids copies

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ object_t func()
 
 int main ()
 {
-  /*document_t document;
+  document_t document;
   
   // reservation does not call object constructor
   document.reserve(5);
@@ -19,14 +19,15 @@ int main ()
   document.emplace_back(3);
   document.emplace_back(5);
 
+  reverse(document.begin(), document.end());
+
   draw(document, std::cout, 0);
 
-  std::cout << "======================\n";
+  /*std::cout << "======================\n";
   object_t x = func();
-*/
   std::cout << "======================\n";
   object_t y = 0;
   y = func();
-
+  */
   return 0;
 }
