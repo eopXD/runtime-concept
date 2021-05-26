bc4163aa21f164541f55de2ea56aa972e246fb40
eopXD <eopXD@skymizer.com>
2021-05-26 16:56:09 +0800

    using emplace_back avoids an extra construction

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ int main ()
 {
   document_t document;
   
+  // reservation does not call object constructor
+  document.reserve(5);
+
+  /*document.emplace_back(1);
+  document.emplace_back(1);
+  document.emplace_back(2);
+  document.emplace_back(3);
+  document.emplace_back(5);
+*/
+
   document.push_back(1);
   document.push_back(1);
   document.push_back(2);
