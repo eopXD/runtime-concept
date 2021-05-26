dc90b708032a8ba345c1bcbae360ead533265a23
eopXD <eopXD@skymizer.com>
2021-05-26 16:36:35 +0800

    apply PIMPL to integer object, data is allocated on head

--- library.h
+++ library.h
@@ -5,16 +5,18 @@
 
 //using object_t = int;
 
-// extract actual implementation as a seperatre function 
+// extract actual implementation as a seperate function
 void draw(const int& obj, std::ostream& out, size_t position)
 {
   out << std::string(position, ' ') << obj << std::endl;
 }
 
-// create a actual class for object_t
+// Create a actual class for object_t. Rewrite object with PIMPL idiom
+// indirection allows underlying private implementation to change and
+// don't need to compile for the original object.
 class object_t
 {
-public:
+/*public:
   object_t(const int& x) : self_(x)
   { }
 
@@ -24,6 +26,29 @@ public:
   }
 private:
   int self_;
+*/
+public:
+  object_t(const int &x) : self_(new int_model_t(x))
+  { }
+  friend void draw(const object_t& obj, std::ostream& out, size_t position)
+  {
+    obj.self_->draw(out, position);
+  }
+private: // private implementaion (PIMPL)
+  struct int_model_t
+  {
+    int_model_t(const int& x) : data_ (x)
+    { }
+    // this method gives caller interface, no underlying data exposed.
+    void draw(std::ostream& out, size_t position) const
+    {
+      ::draw(data_, out, position);
+    }
+    int data_;
+  };
+private:
+// we create data on the heap
+  std::unique_ptr<int_model_t> self_;
 };
 
 using document_t = std::vector<object_t>;
