c36ed7ccfe66f1fb40be9a955614173f448aa34c
eopXD <eopXD@skymizer.com>
2021-05-26 17:56:19 +0800

    able to accept different types of data

--- main.cpp
+++ main.cpp
@@ -13,13 +13,14 @@ int main ()
   // reservation does not call object constructor
   document.reserve(5);
 
+  document.emplace_back(std::string("Fibonacci!"));
   document.emplace_back(1);
   document.emplace_back(1);
   document.emplace_back(2);
   document.emplace_back(3);
   document.emplace_back(5);
 
-  reverse(document.begin(), document.end());
+  //reverse(document.begin(), document.end());
 
   draw(document, std::cout, 0);
 
