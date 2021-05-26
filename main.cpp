8a2c3bba24635bf2ff7c5ef0d647a09631205e23
eopXD <eopXD@skymizer.com>
2021-05-26 16:57:59 +0800

    only 1 constructor is called, this is RVO optimization

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "library.h"
 
+object_t func()
+{
+  object_t result = 5;
+  return result;
+}
+
 int main ()
 {
   document_t document;
@@ -7,20 +13,22 @@ int main ()
   // reservation does not call object constructor
   document.reserve(5);
 
-  /*document.emplace_back(1);
+  document.emplace_back(1);
   document.emplace_back(1);
   document.emplace_back(2);
   document.emplace_back(3);
   document.emplace_back(5);
-*/
 
-  document.push_back(1);
+  /*document.push_back(1);
   document.push_back(1);
   document.push_back(2);
   document.push_back(3);
-  document.push_back(5);
+  document.push_back(5);*/
 
   draw(document, std::cout, 0);
+
+  std::cout << "======================\n";
+  object_t x = func();
 
   return 0;
 }
