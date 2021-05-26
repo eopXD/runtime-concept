6c674ce2459321068106ec4a3e45bba2767551e8
eopXD <eopXD@skymizer.com>
2021-05-26 16:25:45 +0800

    wrap integer into an actual class 'object_t'

--- library.h
+++ library.h
@@ -3,12 +3,28 @@
 #include <string>
 #include <vector>
 
-using object_t = int;
+//using object_t = int;
 
-void draw(const object_t& obj, std::ostream& out, size_t position)
+// extract actual implementation as a seperatre function 
+void draw(const int& obj, std::ostream& out, size_t position)
 {
   out << std::string(position, ' ') << obj << std::endl;
 }
+
+// create a actual class for object_t
+class object_t
+{
+public:
+  object_t(const int& x) : self_(x)
+  { }
+
+  friend void draw(const object_t& obj, std::ostream& out, size_t position)
+  {
+    draw(obj.self_, out, position);
+  }
+private:
+  int self_;
+};
 
 using document_t = std::vector<object_t>;
 
