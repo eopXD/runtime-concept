7746ba7645c2da8fd1bf2d16dce2046c04dbba5c
eopXD <eopXD@skymizer.com>
2021-05-26 17:49:00 +0800

    add new data type: string, so now vector<int_model_t> does not hold, we need to extract a common type for the 2 possible data types. Also for copy constructor, we would need to implement copy functions for both model_t

--- library.h
+++ library.h
@@ -11,6 +11,10 @@ void draw(const int& obj, std::ostream& out, size_t position)
 {
   out << std::string(position, ' ') << obj << std::endl;
 }
+void draw(const std::string& obj, std::ostream& out, size_t position)
+{
+  out << std::string(position, ' ') << obj << std::endl;
+}
 
 // Create a actual class for object_t. Rewrite object with PIMPL idiom
 // indirection allows underlying private implementation to change and
@@ -18,9 +22,12 @@ void draw(const int& obj, std::ostream& out, size_t position)
 class object_t
 {
 public:
+  object_t(std::string s) : self_(new string_model_t(std::move(s)))
+  { std::cout << "ctor\n"; }
+// =============================================================================
   object_t(int x) : self_(new int_model_t(std::move(x)))
   { std::cout << "ctor\n"; }
-  object_t(const object_t& x) : self_(new int_model_t(*x.self_))
+  object_t(const object_t& x) : self_(x.self_->copy_())
   { std::cout << "copy\n"; }
   object_t(object_t&& x) : self_(std::move(x.self_))
   { std::cout << "move\n"; }
@@ -42,15 +49,39 @@ public:
 
   friend void draw(const object_t& obj, std::ostream& out, size_t position)
   {
-    obj.self_->draw(out, position);
+    obj.self_->draw_(out, position);
   }
 private: // private implementaion (PIMPL)
-  struct int_model_t
+  struct concept_t {
+    virtual ~concept_t() = default;
+    virtual concept_t* copy_() const = 0;
+    virtual void draw_(std::ostream&, size_t) const  = 0;
+  };
+  struct string_model_t : concept_t
+  {
+    string_model_t(std::string s) : data_ (std::move(s))
+    { }
+    concept_t* copy_() const
+    {
+      return new string_model_t(*this);
+    }
+    // this method gives caller interface, no underlying data exposed.
+    void draw_(std::ostream& out, size_t position) const
+    {
+      ::draw(data_, out, position);
+    }
+    std::string data_;
+  };
+  struct int_model_t : concept_t
   {
     int_model_t(int x) : data_ (std::move(x))
     { }
+    concept_t* copy_() const
+    {
+      return new int_model_t(*this);
+    }
     // this method gives caller interface, no underlying data exposed.
-    void draw(std::ostream& out, size_t position) const
+    void draw_(std::ostream& out, size_t position) const
     {
       ::draw(data_, out, position);
     }
@@ -58,7 +89,7 @@ private: // private implementaion (PIMPL)
   };
 private:
 // we create data on the heap
-  std::unique_ptr<int_model_t> self_;
+  std::unique_ptr<concept_t> self_;
 };
 
 using document_t = std::vector<object_t>;
